a3d9e56b20c03b7432ef719870df863088f43317
Jack Cheng <chengxianga2008@yahoo.com>
2018-01-18 22:37:09 +1100

    change port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
-app.set('port', process.env.PORT || 8000);
+app.set('port', process.env.PORT || 8080);
 
 var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
